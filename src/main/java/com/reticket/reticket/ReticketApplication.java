9608d68d78463e1fff863c8f232fbec408fa105e
fortzsu <67285950+fortzsu@users.noreply.github.com>
2023-05-07 21:27:48 +0200

    git init

---
+++ src/main/java/com/reticket/reticket/ReticketApplication.java
@@ -0,0 +1,13 @@
+package com.reticket.reticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReticketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReticketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reticket/reticket/ReticketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reticket.reticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
