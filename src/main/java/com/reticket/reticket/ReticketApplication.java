d483c7ca37b1a212239a30948c959ee7a78ca4ed
fortzsu <67285950+fortzsu@users.noreply.github.com>
2023-10-19 11:57:30 +0200

    add @LastModifiedDate

--- src/main/java/com/reticket/reticket/ReticketApplication.java
+++ src/main/java/com/reticket/reticket/ReticketApplication.java
@@ -3,8 +3,10 @@ package com.reticket.reticket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
+@EnableJpaAuditing
 public class ReticketApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/reticket/reticket/domain/TicketActionFollowerEntity.java
+++ src/main/java/com/reticket/reticket/domain/TicketActionFollowerEntity.java
@@ -5,11 +5,17 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "ticket_action_follower_entity")
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 @NoArgsConstructor
@@ -18,8 +24,10 @@ public class TicketActionFollowerEntity {
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "modified_at")
+
+    @LastModifiedDate
     private LocalDateTime modifiedAt;
+
     @Enumerated(EnumType.STRING)
     @Column(name = "ticket_condition")
     private TicketCondition ticketCondition;
